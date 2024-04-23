2722551445b188e2d52e2410fc84a51a2f59526f
Hirok Sengupta <senguptahirok@gmail.com>
2024-04-23 20:22:22 +0530

    debugging

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/api/:user_date',function(req,res){
   let unixTime = ' ';
   if (regex01.test(userD)){
     req.time = new Date(userD).toUTCString();
-    unixTime = new Date().UTC(userD);
+    unixTime = new Date(userD).toLocaleString();
     if (req.time === 'Invalid Date')
       res.send({'error': 'Invalid Date'});
     else res.send({'unix': unixTime, 'utc':req.time});
