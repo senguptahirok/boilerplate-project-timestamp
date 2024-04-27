a3766928c4da7bc6fa6bb72835fa80947a0e1536
Hirok Sengupta <senguptahirok@gmail.com>
2024-04-27 13:58:53 +0530

    debugging

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/api/:user_date',function(req,res){
 
   }
   else if (regex02.test(userD)){
-         let utcD = new Date(userD * 1000).getUTCFullYear();
+         let utcD = new Date(userD).getUTCFullYear();
          res.send({'unix': userD, 'utc': utcD});
         }
         else res.send({'error': 'Invalid Date'});
