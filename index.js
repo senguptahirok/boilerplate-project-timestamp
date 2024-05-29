53e29db8c1596885c2a3840d7d0a35a4251835f4
Hirok Sengupta <senguptahirok@gmail.com>
2024-05-29 09:46:28 +0530

    putting cors after express declaration

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // index.js
 // where your node app starts
 
+// init project
+var express = require('express');
+var app = express();
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// init project
-var express = require('express');
-var app = express();
 /*
 app.get('/api',function(req,res,next){
   req.time = new Date().toString();
