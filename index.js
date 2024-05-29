f8c04092e9fa9820d3afcdd18d254e91c4086189
Hirok Sengupta <senguptahirok@gmail.com>
2024-05-29 09:10:28 +0530

    CORS enable steps moved up

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // index.js
 // where your node app starts
 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
 // init project
 var express = require('express');
 var app = express();
@@ -76,11 +81,6 @@ app.get('/api/:date?',function(req,res){
 //        }
 //        else res.json({'error': 'Invalid Date'});
   });
-
-  // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
